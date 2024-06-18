ef1fa0a48b396bf07c1712a63ea5b71a4ee9ded9
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-06-18 11:37:11 -0300

    characters created

---
+++ src/rpg_modules/characters.js
@@ -0,0 +1,20 @@
+const player1 = {
+    "name": "Araella",
+    "race": "Drow",
+    "class": "Rogue",
+    "strength": 10,
+    "dexterity": 14,
+}
+
+const player2 = {
+    "name": "Arthur",
+    "race": "Human",
+    "class": "Warrior",
+    "strength": 13,
+    "dexterity": 9
+}
+
+module.exports = {
+    player1,
+    player2
+}
